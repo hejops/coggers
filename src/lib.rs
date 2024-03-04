f4f16dd8086e83556f4da0e88636b87f5cc49bac
Joseph <hejops1@gmail.com>
2024-03-04 21:56:28 +0000

    add main structs

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,19 @@ mod tests {
         // https://www.discogs.com/release/8196883
         let rel: Release = get_release(8196883).unwrap();
         assert_eq!(rel.year, 1998);
+        assert_eq!(rel.uri, "https://www.discogs.com/release/8196883-Monica-Groop-Ostrobothnian-Chamber-Orchestra-Conductor-Juha-Kangas-Bach-Alto-Cantatas");
         assert_eq!(rel.id, 8196883);
         assert_eq!(rel.genres, vec!["Classical"]);
+        assert_eq!(rel.artists[0].name, "Monica Groop");
+
+        assert_eq!(
+            rel.tracklist[0].title,
+            "J.S.Bach: Cantatas BWV 170 / 35 / 169"
+        );
+        assert_eq!(
+            rel.tracklist[1].sub_tracks.as_ref().unwrap()[0].title,
+            "Aria \"Vergnügte Ruh, Beliebte Seelenlust\"",
+        );
     }
 
     #[test]
--- src/release.rs
+++ src/release.rs
@@ -4,11 +4,58 @@ use serde::Serialize;
 use crate::http;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Track {
+    pub title: String,
+    pub duration: String,
+    pub type_: String,
+    pub sub_tracks: Option<Vec<Track>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Label {
+    pub id: usize,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Artist {
+    // TODO: empty strings should be deserialised to None
+    pub anv: String,
+    pub id: usize,
+    pub name: String,
+    pub resource_url: String,
+    pub role: String,
+    pub tracks: String, // this being a String is very problematic
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Release {
-    pub id: u32,
-    pub year: u32,
-    pub uri: String,
+    pub blocked_from_sale: bool,
+    pub id: usize, // u32 is probably fine
+    pub year: u16,
+
+    pub data_quality: String, // TODO: enum
+    pub status: String,       // TODO: enum
+
+    pub resource_url: String, // api.discogs.com
+    pub uri: String,          // www.discogs.com
+
+    pub artists_sort: String,
+    pub country: String,
     pub genres: Vec<String>,
+    pub notes: String,
+    pub title: String,
+
+    pub artists: Vec<Artist>,
+    pub extraartists: Vec<Artist>,
+    pub labels: Vec<Label>,
+    pub tracklist: Vec<Track>,
+
+    // companies: Vec,
+    // formats: Vec,
+    // identifiers: Vec,
+    lowest_price: f32,
+    num_for_sale: usize,
 }
 
 pub fn get_release(release_id: usize) -> Option<Release> {
