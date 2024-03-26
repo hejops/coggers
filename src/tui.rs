69700466eed25e6cbbc1b48b36f01131d8b58e41
Joseph <hejops1@gmail.com>
2024-03-26 21:13:26 +0000

    add trait Menu

--- src/tui.rs
+++ src/tui.rs
@@ -11,7 +11,9 @@ use crossterm::terminal::LeaveAlternateScreen;
 use crossterm::ExecutableCommand;
 use ratatui::prelude::*;
 use ratatui::widgets::*;
+use walkdir::DirEntry;
 
+use crate::io::Walk;
 use crate::release::Release;
 
 // https://github.com/ratatui-org/ratatui/tree/main?tab=readme-ov-file#example
@@ -34,8 +36,11 @@ fn handle_events() -> io::Result<bool> {
     Ok(false)
 }
 
-impl Release {
-    pub fn menu(&self) -> io::Result<()> {
+pub trait Menu {
+    /// Responsible for the `ratatui` loop, and controlled by an event handler.
+    /// The event handler is currently implemented globally; this will
+    /// probably become a trait impl.
+    fn menu(&self) -> io::Result<()> {
         enable_raw_mode()?;
         stdout().execute(EnterAlternateScreen)?;
         let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
@@ -44,7 +49,7 @@ impl Release {
 
         // any data fetched within this loop must be cached -- time to use HashMap
         while !should_quit {
-            terminal.draw(|frame| Self::ui(frame, self))?;
+            terminal.draw(|frame| Self::render(frame, self))?;
             should_quit = handle_events()?;
         }
 
@@ -53,20 +58,54 @@ impl Release {
         Ok(())
     }
 
-    fn ui(
+    /// Responsible for rendering a single 'frame' in `menu`. Implementation
+    /// will vary depending on the data structure, and the intended widget to be
+    /// rendered.
+    fn render(
+        frame: &mut Frame,
+        content: &Self,
+    );
+}
+
+impl Menu for Release {
+    fn render(
         frame: &mut Frame,
         rel: &Self,
     ) {
-        // para surrounded by block
         // https://docs.rs/ratatui/0.26.1/ratatui/widgets/trait.Widget.html
+
+        // para surrounded by block -- not very stateful, and not a good representation
+        // of the data
         // let para = Paragraph::new(rel.display_tracklist());
 
         let items = rel.parse_tracklist().into_iter().map(|t| t.to_string());
         let list = List::new(items);
 
+        // TODO: https://docs.rs/ratatui/0.26.1/ratatui/widgets/struct.ListState.html
+
+        // not essential
         let block = Block::default()
             .title(rel.to_string())
             .borders(Borders::TOP);
+
+        frame.render_widget(list.block(block), frame.size());
+    }
+}
+
+impl Menu for DirEntry {
+    fn render(
+        frame: &mut Frame,
+        dir: &Self,
+    ) {
+        // https://docs.rs/ratatui/0.26.1/ratatui/widgets/trait.Widget.html
+
+        let entries: Vec<_> = dir
+            .walk()
+            .map(|d| d.path().to_str().unwrap().to_string())
+            .collect();
+        let list = List::new(entries);
+
+        let block = Block::default().borders(Borders::TOP);
 
         frame.render_widget(list.block(block), frame.size());
     }
