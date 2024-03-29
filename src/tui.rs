401bfdf42b678f41eb9fa80a35ebb605d8bfcad0
Joseph <hejops1@gmail.com>
2024-03-29 23:12:49 +0000

    implement tagger tui with basic scrolling

--- src/tui.rs
+++ src/tui.rs
@@ -1,9 +1,13 @@
+//! TUI implementation for tagging files in the source directory. Heavily
+//! borrowed from the [ratatui list example](https://docs.rs/ratatui/latest/src/list/list.rs.html).
+
 use std::io;
 use std::io::stdout;
 
 use crossterm::event;
 use crossterm::event::Event;
 use crossterm::event::KeyCode;
+use crossterm::event::KeyEventKind;
 use crossterm::terminal::disable_raw_mode;
 use crossterm::terminal::enable_raw_mode;
 use crossterm::terminal::EnterAlternateScreen;
@@ -12,195 +16,122 @@ use crossterm::ExecutableCommand;
 use ratatui::prelude::*;
 use ratatui::widgets::*;
 use walkdir::DirEntry;
-use walkdir::WalkDir;
 
-use crate::io::Walk;
-use crate::release::Release;
+use crate::io::Sort;
 
-// https://github.com/ratatui-org/ratatui/tree/main?tab=readme-ov-file#example
+pub struct TaggerApp {
+    state: ListState,
+    items: Vec<String>, // TODO: &str (lifetimes...)
+    last_selected: Option<usize>,
+}
 
-fn handle_events() -> io::Result<bool> {
-    if !event::poll(std::time::Duration::from_millis(50))? {
-        return Ok(false);
-    };
-    if let Event::Key(key) = event::read()? {
-        if key.kind != event::KeyEventKind::Press {
-            return Ok(false);
-        }
-        match key.code {
-            // what if we allowed spawning a new .menu()?
-            KeyCode::Char('q') => return Ok(true),
-            KeyCode::Char('x') => return Ok(true),
-            _ => (),
+impl TaggerApp {
+    pub fn with_items(items: Vec<DirEntry>) -> Self {
+        TaggerApp {
+            state: ListState::default(), //.with_offset(3),
+            items: items.iter().map(|f| f.as_str().to_string()).collect(),
+            last_selected: None,
         }
     }
 
-    Ok(false)
-}
-
-pub trait Menu {
-    /// Responsible for the `ratatui` loop, and controlled by an event handler.
-    /// The event handler is currently implemented globally; this will
-    /// probably become a trait impl.
-    fn menu(&mut self) -> io::Result<()> {
+    /// `main` -> `run` -> loop{`draw` -> `render` -> `render` components...}
+    pub fn main(&mut self) -> io::Result<()> {
         enable_raw_mode()?;
         stdout().execute(EnterAlternateScreen)?;
-        let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-
-        let mut should_quit = false;
-
-        // any data fetched within this loop must be cached -- time to use HashMap
-        while !should_quit {
-            terminal.draw(|frame| Self::render(self, frame))?;
-            // should_quit = handle_events()?;
-            should_quit = self.get_new_state()?;
-        }
-
+        let terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+        self.run(terminal)?;
         disable_raw_mode()?;
         stdout().execute(LeaveAlternateScreen)?;
         Ok(())
     }
 
-    // default impl is somewhat pointless, as it can only quit. in order to do
-    // anything stateful, we need to know the implementor's state, which we, as
-    // the trait, cannot know anything about
-    fn get_new_state(&mut self) -> io::Result<bool> {
-        // no events detected, do nothing
-        if !event::poll(std::time::Duration::from_millis(50))? {
-            return Ok(false);
-        };
-        if let Event::Key(key) = event::read()? {
-            if key.kind != event::KeyEventKind::Press {
-                return Ok(false);
-            }
-            match key.code {
-                KeyCode::Char('q') => return Ok(true),
-                KeyCode::Char('x') => return Ok(true),
-                _ => (),
+    fn run(
+        &mut self,
+        mut terminal: Terminal<impl Backend>,
+    ) -> io::Result<()> {
+        loop {
+            self.draw(&mut terminal)?;
+
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    use KeyCode::*;
+                    match key.code {
+                        Char('q') | Esc => return Ok(()),
+                        // Char('h') | Left => self.items.unselect(),
+                        Char('j') | Down => self.next(),
+                        Char('k') | Up => self.previous(),
+                        // Char('l') | Right | Enter => self.change_status(),
+                        // Char('g') => self.go_top(),
+                        // Char('G') => self.go_bottom(),
+                        _ => {}
+                    }
+                }
             }
         }
-
-        Ok(false)
     }
 
-    /// Responsible for rendering a single 'frame' in `menu`. Implementation
-    /// will vary depending on the data structure, and the intended widget to be
-    /// rendered.
-    fn render(
+    fn draw(
         &mut self,
-        frame: &mut Frame,
-    );
-}
-
-impl Menu for Release {
-    fn render(
-        &mut self,
-        frame: &mut Frame,
-    ) {
-        // https://docs.rs/ratatui/0.26.1/ratatui/widgets/trait.Widget.html
-
-        // para surrounded by block -- not very stateful, and not a good representation
-        // of the data
-        // let para = Paragraph::new(rel.display_tracklist());
-
-        let items = self.parse_tracklist().into_iter().map(|t| t.to_string());
-        let list = List::new(items);
-
-        // TODO: https://docs.rs/ratatui/0.26.1/ratatui/widgets/struct.ListState.html
-
-        // not essential
-        let block = Block::default()
-            .title(self.to_string())
-            .borders(Borders::TOP);
-
-        frame.render_widget(list.block(block), frame.size());
-    }
-}
-
-impl Menu for DirEntry {
-    fn render(
-        &mut self,
-        frame: &mut Frame,
-    ) {
-        // https://docs.rs/ratatui/0.26.1/ratatui/widgets/trait.Widget.html
-
-        let entries: Vec<_> = self
-            .walk()
-            .map(|d| d.path().to_str().unwrap().to_string())
-            .collect();
-        let list = List::new(entries);
-
-        let block = Block::default().borders(Borders::TOP);
-        frame.render_widget(list.block(block), frame.size());
-
-        // let mut state = ListState::default(); //.with_offset(1);
-        // frame.render_stateful_widget(list.block(block), frame.size(), &mut
-        // state);
-    }
-}
-
-pub struct DirMenu {
-    dirs: Vec<DirEntry>,
-    state: ListState,
-}
-
-impl DirMenu {
-    pub fn new(root: &str) -> Self {
-        let dirs = WalkDir::new(root)
-            .into_iter()
-            .filter_map(|f| f.ok())
-            .collect();
-        let state = ListState::default(); //.with_offset(1);
-        Self { dirs, state }
-    }
-}
-impl Menu for DirMenu {
-    fn render(
-        &mut self,
-        frame: &mut Frame,
-    ) {
-        let entries: Vec<_> = self
-            .dirs
-            .iter()
-            .map(|d| d.path().to_str().unwrap().to_string())
-            .collect();
-        let list = List::new(entries);
-
-        let block = Block::default().borders(Borders::TOP);
-
-        frame.render_stateful_widget(list.block(block), frame.size(), &mut self.state);
-
-        // https://docs.rs/ratatui/0.26.1/src/demo2/tabs/email.rs.html#97
+        terminal: &mut Terminal<impl Backend>,
+    ) -> io::Result<()> {
+        terminal.draw(|f| f.render_widget(self, f.size()))?;
+        Ok(())
     }
 
-    fn get_new_state(&mut self) -> io::Result<bool> {
-        // no events detected, do nothing
-        if !event::poll(std::time::Duration::from_millis(50))? {
-            return Ok(false);
+    // state management
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => self.last_selected.unwrap_or(0),
         };
-        if let Event::Key(key) = event::read()? {
-            if key.kind != event::KeyEventKind::Press {
-                return Ok(false);
-            }
-            match key.code {
-                KeyCode::Char('q') => return Ok(true),
-                KeyCode::Char('x') => return Ok(true),
-                KeyCode::Char('j') => {
-                    if self.state.offset() < self.dirs.len() {
-                        *self.state.offset_mut() += 1;
-                    }
-                    return noquit;
-                }
-                KeyCode::Char('k') => {
-                    if self.state.offset() > 0 {
-                        *self.state.offset_mut() -= 1;
-                    }
-                    return noquit;
-                }
-                _ => (),
-            }
-        }
+        self.state.select(Some(i));
+    }
 
-        Ok(false)
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => self.last_selected.unwrap_or(0),
+        };
+        self.state.select(Some(i));
+    }
+}
+
+// rendering
+
+impl Widget for &mut TaggerApp {
+    fn render(
+        self,
+        area: Rect,
+        buf: &mut Buffer,
+    ) where
+        Self: Sized,
+    {
+        self.render_items(area, buf);
+    }
+}
+
+impl TaggerApp {
+    pub fn render_items(
+        &mut self,
+        area: Rect,
+        buf: &mut Buffer,
+    ) {
+        // note: highlight only becomes visible when an item is selected
+        // TODO: don't clone
+        let list = List::new(self.items.clone()).highlight_symbol("> ");
+        StatefulWidget::render(list, area, buf, &mut self.state);
     }
 }
